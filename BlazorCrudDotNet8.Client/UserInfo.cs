2301dec31ae1047a1e8e95caea0173595a5ad78a
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-16 13:16:48 -0600

    dotnet format

--- BlazorCrudDotNet8.Client/UserInfo.cs
+++ BlazorCrudDotNet8.Client/UserInfo.cs
@@ -1,4 +1,4 @@
-namespace BlazorCrudDotNet8.Client;
+﻿namespace BlazorCrudDotNet8.Client;
 
 // Add properties to this class and update the server and client AuthenticationStateProviders
 // to expose more information about the authenticated user to the client.
