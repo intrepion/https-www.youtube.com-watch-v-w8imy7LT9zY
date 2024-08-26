2cfb39c54b8bd07aa5dcd7f0aa9d38b085be7449
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-25 20:06:17 -0600

    cd BlazorCrudDotNet8 && dotnet ef --project ../BlazorCrudDotNet8.Shared migrations add Games && cd ..

--- BlazorCrudDotNet8.Shared/Migrations/20240826020244_Games.Designer.cs
+++ BlazorCrudDotNet8.Shared/Migrations/20240826020244_Games.Designer.cs
@@ -4,6 +4,7 @@ using BlazorCrudDotNet8.Shared.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorCrudDotNet8.Shared.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240826020244_Games")]
+    partial class Games
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
