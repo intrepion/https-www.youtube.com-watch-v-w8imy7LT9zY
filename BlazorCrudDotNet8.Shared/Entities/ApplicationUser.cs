b980631c2a2ae102ca0a63d0dc183102c468081c
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-28 13:35:40 -0600

    cp -r ../intrepion-bash/dotnet/files/AppNamePlaceholder.Shared/* BlazorCrudDotNet8.Shared/

---
+++ BlazorCrudDotNet8.Shared/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AppNamePlaceholder.Shared.Entities;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public ICollection<ApplicationRole>? UpdatedApplicationRoles { get; set; }
+    public ICollection<ApplicationUser>? UpdatedApplicationUsers { get; set; }
+}
---
+++ BlazorCrudDotNet8.Shared/Entities/ApplicationUserEntityTypeConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AppNamePlaceholder.Shared.Entities;
+
+public class ApplicationUserEntityTypeConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder
+            .ToTable("AspNetUsers", x => x.IsTemporal());
+
+        builder.Property(x => x.Email)
+            .IsRequired();
+
+        builder.Property(x => x.NormalizedEmail)
+            .IsRequired();
+
+        builder.Property(x => x.NormalizedUserName)
+            .IsRequired();
+
+        builder.Property(x => x.UserName)
+            .IsRequired();
+
+        builder.HasIndex(x => x.NormalizedUserName)
+            .IsUnique();
+
+        builder
+            .HasOne(x => x.ApplicationUserUpdatedBy)
+            .WithMany(x => x.UpdatedApplicationUsers)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
