364c6ac62919ce42599025a46e1888d7c23ae19d
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-04 19:33:25 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Services/IEntityNamePlaceholderAdminService.cs BlazorCrudDotNet8.BusinessLogic/Services/IGameAdminService.cs

---
+++ BlazorCrudDotNet8.BusinessLogic/Services/IGameAdminService.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationNamePlaceholder.BusinessLogic.Entities;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Services;
+
+public interface IEntityNamePlaceholderAdminService
+{
+    Task<EntityNamePlaceholder?> AddAsync(string userName, EntityNamePlaceholder LowercaseNamePlaceholder);
+    Task<bool> DeleteAsync(string userName, Guid id);
+    Task<EntityNamePlaceholder?> EditAsync(string userName, Guid id, EntityNamePlaceholder LowercaseNamePlaceholder);
+    Task<List<EntityNamePlaceholder>?> GetAllAsync();
+    Task<EntityNamePlaceholder?> GetByIdAsync(Guid id);
+}
