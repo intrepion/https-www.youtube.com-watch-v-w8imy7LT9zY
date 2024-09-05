2889b717ebc8c8875ec78e847d396227e4b6dc26
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-04 19:33:45 -0600

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/EntityNamePlaceholder.cs BlazorCrudDotNet8.BusinessLogic/Entities/Game.cs

---
+++ BlazorCrudDotNet8.BusinessLogic/Entities/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
+
+public class EntityNamePlaceholder
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public Guid Id { get; set; }
+    // public string PropertyNamePlaceholder { get; set; } = string.Empty;
+}
