d9b6a812d2e06982549a06ec3283f93e9c994c27
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-04 19:33:40 -0600

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"game\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Services/IGameAdminService.cs

--- BlazorCrudDotNet8.BusinessLogic/Services/IGameAdminService.cs
+++ BlazorCrudDotNet8.BusinessLogic/Services/IGameAdminService.cs
@@ -4,9 +4,9 @@ namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
 public interface IGameAdminService
 {
-    Task<Game?> AddAsync(string userName, Game LowercaseNamePlaceholder);
+    Task<Game?> AddAsync(string userName, Game game);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<Game?> EditAsync(string userName, Guid id, Game LowercaseNamePlaceholder);
+    Task<Game?> EditAsync(string userName, Guid id, Game game);
     Task<List<Game>?> GetAllAsync();
     Task<Game?> GetByIdAsync(Guid id);
 }
