3ac5cd19fe9e4974f17ed47707c5caa8d0d2251a
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-04 19:20:35 -0600

    dotnet format

--- BlazorCrudDotNet8/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ BlazorCrudDotNet8/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -1,14 +1,14 @@
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Text.Json;
+using BlazorCrudDotNet8.Components.Account.Pages;
+using BlazorCrudDotNet8.Components.Account.Pages.Manage;
+using BlazorCrudDotNet8.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
-using BlazorCrudDotNet8.Components.Account.Pages;
-using BlazorCrudDotNet8.Components.Account.Pages.Manage;
-using BlazorCrudDotNet8.Data;
 
 namespace Microsoft.AspNetCore.Routing;
 
