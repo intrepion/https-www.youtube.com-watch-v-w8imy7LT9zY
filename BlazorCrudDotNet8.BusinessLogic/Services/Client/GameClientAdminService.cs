b97cc1a6bceaee554335fbc2a104142888ab86f9
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-19 21:28:57 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Game\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Services/Client/GameClientAdminService.cs

--- BlazorCrudDotNet8.BusinessLogic/Services/Client/GameClientAdminService.cs
+++ BlazorCrudDotNet8.BusinessLogic/Services/Client/GameClientAdminService.cs
@@ -3,15 +3,15 @@ using ApplicationNamePlaceholder.BusinessLogic.Entities.DataTransferObjects;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Services.Client;
 
-public class EntityNamePlaceholderClientAdminService(HttpClient httpClient) : IEntityNamePlaceholderAdminService
+public class GameClientAdminService(HttpClient httpClient) : IGameAdminService
 {
     private readonly HttpClient _httpClient = httpClient;
 
-    public async Task<EntityNamePlaceholderAdminDataTransferObject?> AddAsync(string userName, EntityNamePlaceholderAdminDataTransferObject gameAdminDataTransferObject)
+    public async Task<GameAdminDataTransferObject?> AddAsync(string userName, GameAdminDataTransferObject gameAdminDataTransferObject)
     {
         var result = await _httpClient.PostAsJsonAsync("/api/admin/gameAdmin", gameAdminDataTransferObject);
 
-        return await result.Content.ReadFromJsonAsync<EntityNamePlaceholderAdminDataTransferObject>();
+        return await result.Content.ReadFromJsonAsync<GameAdminDataTransferObject>();
     }
 
     public async Task<bool> DeleteAsync(string userName, Guid id)
@@ -21,23 +21,23 @@ public class EntityNamePlaceholderClientAdminService(HttpClient httpClient) : IE
         return await result.Content.ReadFromJsonAsync<bool>();
     }
 
-    public async Task<EntityNamePlaceholderAdminDataTransferObject?> EditAsync(string userName, Guid id, EntityNamePlaceholderAdminDataTransferObject gameAdminDataTransferObject)
+    public async Task<GameAdminDataTransferObject?> EditAsync(string userName, Guid id, GameAdminDataTransferObject gameAdminDataTransferObject)
     {
         var result = await _httpClient.PutAsJsonAsync($"/api/admin/gameAdmin/{id}", gameAdminDataTransferObject);
 
-        return await result.Content.ReadFromJsonAsync<EntityNamePlaceholderAdminDataTransferObject>();
+        return await result.Content.ReadFromJsonAsync<GameAdminDataTransferObject>();
     }
 
-    public async Task<List<EntityNamePlaceholderAdminDataTransferObject>?> GetAllAsync()
+    public async Task<List<GameAdminDataTransferObject>?> GetAllAsync()
     {
-        var result = await _httpClient.GetFromJsonAsync<List<EntityNamePlaceholderAdminDataTransferObject>>("/api/admin/gameAdmin");
+        var result = await _httpClient.GetFromJsonAsync<List<GameAdminDataTransferObject>>("/api/admin/gameAdmin");
 
         return result;
     }
 
-    public async Task<EntityNamePlaceholderAdminDataTransferObject?> GetByIdAsync(Guid id)
+    public async Task<GameAdminDataTransferObject?> GetByIdAsync(Guid id)
     {
-        var result = await _httpClient.GetFromJsonAsync<EntityNamePlaceholderAdminDataTransferObject>($"/api/admin/gameAdmin/{id}");
+        var result = await _httpClient.GetFromJsonAsync<GameAdminDataTransferObject>($"/api/admin/gameAdmin/{id}");
 
         return result;
     }
