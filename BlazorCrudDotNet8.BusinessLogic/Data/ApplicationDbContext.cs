ff3f4f1bfe6e7ce3ab6cbe3751b304cea5a6aa95
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-22 23:41:12 -0600

    gawk -i inplace "{ gsub(/\/\/ EntityTypeCfgCodePlaceholder/, \"new GameEntityTypeConfiguration().Configure(builder.Entity<Game>());\n\ \ \ \ \ \ \ \ // EntityTypeCfgCodePlaceholder\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Data/ApplicationDbContext.cs

--- BlazorCrudDotNet8.BusinessLogic/Data/ApplicationDbContext.cs
+++ BlazorCrudDotNet8.BusinessLogic/Data/ApplicationDbContext.cs
@@ -22,6 +22,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         new ApplicationUserRoleEntityTypeConfiguration().Configure(builder.Entity<ApplicationUserRole>());
         new ApplicationUserTokenEntityTypeConfiguration().Configure(builder.Entity<ApplicationUserToken>());
 
+        new GameEntityTypeConfiguration().Configure(builder.Entity<Game>());
         // EntityTypeCfgCodePlaceholder
     }
 }
