5c27709491e599b8f2f8e0da5be6b37ab5783ada
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 23:03:14 -0600

    gawk -i inplace "{ gsub(/\/\/ AddDatabasePropertyCodePlaceholder/, \"game.NormalizedName = gameAdminDto.Name.ToUpperInvariant();\n\ \ \ \ \ \ \ \ // AddDatabasePropertyCodePlaceholder\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Services/Server/GameAdminService.cs

--- BlazorCrudDotNet8.BusinessLogic/Services/Server/GameAdminService.cs
+++ BlazorCrudDotNet8.BusinessLogic/Services/Server/GameAdminService.cs
@@ -31,6 +31,7 @@ public class GameAdminService(ApplicationDbContext applicationDbContext) : IGame
 
         var game = GameAdminDto.ToGame(user, gameAdminDto);
 
+        game.NormalizedName = gameAdminDto.Name.ToUpperInvariant();
         // AddDatabasePropertyCodePlaceholder
 
         var result = await _applicationDbContext.Games.AddAsync(game);
