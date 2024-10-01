4b29d8a54ddbf578ab172dc0c7b6c4f9c206c158
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-01 06:59:10 -0600

    Got all acceptance tests working!

--- BlazorCrudDotNet8.AcceptanceTests/Admin/ApplicationRoleAdminPages.cs
+++ BlazorCrudDotNet8.AcceptanceTests/Admin/ApplicationRoleAdminPages.cs
@@ -1,27 +1,27 @@
-﻿using Microsoft.Playwright;
+using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
 namespace BlazorCrudDotNet8.AcceptanceTests.Admin;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
-public partial class GameAdminPages : PageTest
+public partial class ApplicationRoleAdminPages : PageTest
 {
     [Test]
     public async Task MainNavigation()
     {
-        await Page.GetByRole(AriaRole.Link, new() { Name = "Games" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("Game List");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Application Roles" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Application Role List");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("Game Creation");
-        await Page.GetByTestId("gameAdminEditName").FillAsync("a game");
+        await Expect(Page).ToHaveTitleAsync("Application Role Creation");
+        await Page.GetByTestId("applicationRoleAdminEditName").FillAsync("a applicationRole");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("Game Modification");
-        await Page.GetByTestId("gameAdminEditName").FillAsync("some game");
+        await Expect(Page).ToHaveTitleAsync("Application Role Modification");
+        await Page.GetByTestId("applicationRoleAdminEditName").FillAsync("some applicationRole");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Modify" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("Game Modification");
+        await Expect(Page).ToHaveTitleAsync("Application Role Modification");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Remove" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("Game List");
+        await Expect(Page).ToHaveTitleAsync("Application Role List");
     }
 
     [SetUp]
