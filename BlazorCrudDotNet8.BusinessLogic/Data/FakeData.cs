f3aa52195b67a5690ff84de0edd0afd95b91df0c
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-04 00:09:22 -0600

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorCrudDotNet8\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Data/*

--- BlazorCrudDotNet8.BusinessLogic/Data/FakeData.cs
+++ BlazorCrudDotNet8.BusinessLogic/Data/FakeData.cs
@@ -1,16 +1,16 @@
 ﻿using Bogus;
-using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using BlazorCrudDotNet8.BusinessLogic.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Data;
+namespace BlazorCrudDotNet8.BusinessLogic.Data;
 
 public static class FakeData
 {
     public static async Task SeedAsync(ApplicationDbContext applicationDbContext, IServiceProvider serviceProvider)
     {
         var adminName = "Admin";
-        var adminUserPass = adminName + "1@ApplicationNamePlaceholder.com";
+        var adminUserPass = adminName + "1@BlazorCrudDotNet8.com";
         var adminUser = (await applicationDbContext.Users.AddAsync(new ApplicationUser
         {
             Email = adminUserPass,
