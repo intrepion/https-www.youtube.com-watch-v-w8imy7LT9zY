a67ac2b763e096ceb153269b466de53639ae443d
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-06 17:19:38 -0600

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorCrudDotNet8\") }; { print }" BlazorCrudDotNet8/Controllers/*

--- BlazorCrudDotNet8/Controllers/GameAdminController.cs
+++ BlazorCrudDotNet8/Controllers/GameAdminController.cs
@@ -1,8 +1,8 @@
-﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
-using ApplicationNamePlaceholder.BusinessLogic.Services;
+﻿using BlazorCrudDotNet8.BusinessLogic.Entities.Dtos;
+using BlazorCrudDotNet8.BusinessLogic.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ApplicationNamePlaceholder.Controllers;
+namespace BlazorCrudDotNet8.Controllers;
 
 [Route("api/admin/[controller]")]
 [ApiController]
