732f2354add7dbeb9c89631987371515e4b10bac
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-06 17:19:08 -0600

    gawk -i inplace "{ gsub(/PluralNamePlaceholder/, \"Games\") }; { print }" BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs

--- BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs
+++ BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs
@@ -11,7 +11,7 @@ public partial class GameAdminPages : PageTest
     public async Task MainNavigation()
     {
         await Expect(Page).ToHaveTitleAsync("Home");
-        await Page.GetByRole(AriaRole.Link, new() { Name = "PluralNamePlaceholder" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Games" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Game List");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Game Creation");
