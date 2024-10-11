b39bd84c9749c0877202913344814b4ba217f200
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-11 14:58:45 -0600

    gawk -i inplace "{ gsub(/HumanNamePlaceholder/, \"Game\") }; { print }" BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs

--- BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs
+++ BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs
@@ -13,23 +13,23 @@ public partial class GameAdminPages : PageTest
         await Expect(Page).ToHaveTitleAsync("Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "PluralNamePlaceholder" }).ClickAsync();
         await Page.GetByTestId("gameNavLink").ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder List");
+        await Expect(Page).ToHaveTitleAsync("Game List");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Creation");
+        await Expect(Page).ToHaveTitleAsync("Game Creation");
 
         // CreatePropertyCodePlaceholder
         // await Page.GetByTestId("gameAdminEditName").FillAsync("a game");
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Modification");
+        await Expect(Page).ToHaveTitleAsync("Game Modification");
 
         // ModifyPropertyCodePlaceholder
         // await Page.GetByTestId("gameAdminEditName").FillAsync("some game");
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Modify" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Modification");
+        await Expect(Page).ToHaveTitleAsync("Game Modification");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Remove" }).ClickAsync();
-        await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder List");
+        await Expect(Page).ToHaveTitleAsync("Game List");
     }
 
     [SetUp]
