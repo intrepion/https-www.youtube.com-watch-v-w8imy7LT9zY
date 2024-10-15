d68922d515ffa94aa1ebec01e71e87096a4f50e3
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-15 10:40:07 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"game\") }; { print }" BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs

--- BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs
+++ BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs
@@ -11,19 +11,19 @@ public partial class EntityNamePlaceholderAdminPages : PageTest
     public async Task MainNavigation()
     {
         await Expect(Page).ToHaveTitleAsync("Home");
-        await Page.GetByTestId("EntityLowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("gameNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder List");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Creation");
 
         // CreatePropertyCodePlaceholder
-        // await Page.GetByTestId("EntityLowercaseNamePlaceholderAdminEditName").FillAsync("a EntityLowercaseNamePlaceholder");
+        // await Page.GetByTestId("gameAdminEditName").FillAsync("a game");
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Modification");
 
         // ModifyPropertyCodePlaceholder
-        // await Page.GetByTestId("EntityLowercaseNamePlaceholderAdminEditName").FillAsync("some EntityLowercaseNamePlaceholder");
+        // await Page.GetByTestId("gameAdminEditName").FillAsync("some game");
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Modify" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("HumanNamePlaceholder Modification");
