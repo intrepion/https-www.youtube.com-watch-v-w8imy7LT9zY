0b4178cb766c412e889bd012038a538a486952a2
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-16 23:38:42 -0600

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public string NormalizedName { get; set; } = string.Empty;\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Entities/Game.cs

--- BlazorCrudDotNet8.BusinessLogic/Entities/Game.cs
+++ BlazorCrudDotNet8.BusinessLogic/Entities/Game.cs
@@ -6,5 +6,6 @@ public class Game
     public Guid Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
+    public string NormalizedName { get; set; } = string.Empty;
     // ActualPropertyPlaceholder
 }
