203139cbf56a14250ee5668acd6516f43572869a
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-16 23:38:51 -0600

    gawk -i inplace "{ gsub(/\/\/ JustModelPropertyPlaceholder/, \"public string Name { get; set; } = string.Empty;\n\ \ \ \ // JustModelPropertyPlaceholder\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Entities/Models/GameAdminEditModel.cs

--- BlazorCrudDotNet8.BusinessLogic/Entities/Models/GameAdminEditModel.cs
+++ BlazorCrudDotNet8.BusinessLogic/Entities/Models/GameAdminEditModel.cs
@@ -6,6 +6,7 @@ public class GameAdminEditModel
 {
     public Guid Id { get; set; }
 
+    public string Name { get; set; } = string.Empty;
     // JustModelPropertyPlaceholder
 
     public static GameAdminEditModel FromGameAdminDto(GameAdminDto gameAdminDto)
