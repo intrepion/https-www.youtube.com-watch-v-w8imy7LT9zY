ac2b1db43e178e2526a56ee1450289f055423ade
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-16 23:39:06 -0600

    rm -rf BlazorCrudDotNet8/Data

--- BlazorCrudDotNet8/Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace BlazorCrudDotNet8.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-{
-}
