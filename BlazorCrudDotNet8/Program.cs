9b4829161e4cf202a6a41b3954fe0a008484247b
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-17 22:02:15 -0600

    dotnet aspnet-codegenerator blazor CRUD --dataContext ApplicationDbContext --model ApplicationRole

--- BlazorCrudDotNet8/Program.cs
+++ BlazorCrudDotNet8/Program.cs
@@ -1,4 +1,4 @@
-﻿using BlazorCrudDotNet8.BusinessLogic.Data;
+using BlazorCrudDotNet8.BusinessLogic.Data;
 using BlazorCrudDotNet8.BusinessLogic.Entities;
 using BlazorCrudDotNet8.Client.Pages;
 using BlazorCrudDotNet8.Components;
@@ -41,6 +41,8 @@ else
         options.UseSqlServer(connectionString));
 }
 
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -79,6 +81,7 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
