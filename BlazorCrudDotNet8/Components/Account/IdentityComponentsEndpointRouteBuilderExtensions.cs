84e8432e16731fd6dd380fd5f95acbccb00a04a0
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-17 22:01:33 -0600

    cp -r ../intrepion-bash/languages/csharp/dotnet/files/app/ApplicationNamePlaceholder/* BlazorCrudDotNet8/

--- BlazorCrudDotNet8/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ BlazorCrudDotNet8/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -1,8 +1,8 @@
 ﻿using System.Security.Claims;
 using System.Text.Json;
-using BlazorCrudDotNet8.Components.Account.Pages;
-using BlazorCrudDotNet8.Components.Account.Pages.Manage;
-using BlazorCrudDotNet8.Data;
+using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using ApplicationNamePlaceholder.Components.Account.Pages;
+using ApplicationNamePlaceholder.Components.Account.Pages.Manage;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
