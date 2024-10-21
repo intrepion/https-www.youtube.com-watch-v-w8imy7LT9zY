80f096ba446738f3f66384a3929ab04247340707
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-21 03:40:29 -0600

    gawk -i inplace "{ gsub(/\/\/ EditRequiredPropertyCodePlaceholder/, \"if (string.IsNullOrWhiteSpace(gameAdminDto.Name))\n\ \ \ \ \ \ \ \ {\n\ \ \ \ \ \ \ \ \ \ \ \ throw new Exception(\\\"Name required.\\\");\n\ \ \ \ \ \ \ \ }\n\n\ \ \ \ \ \ \ \ // EditRequiredPropertyCodePlaceholder\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Repositories/Admin/Server/GameAdminRepository.cs

--- BlazorCrudDotNet8.BusinessLogic/Repositories/Admin/Server/GameAdminRepository.cs
+++ BlazorCrudDotNet8.BusinessLogic/Repositories/Admin/Server/GameAdminRepository.cs
@@ -93,6 +93,11 @@ public class GameAdminRepository(ApplicationDbContext applicationDbContext) : IG
             throw new Exception("HumanNamePlaceholder not found.");
         }
 
+        if (string.IsNullOrWhiteSpace(gameAdminDto.Name))
+        {
+            throw new Exception("Name required.");
+        }
+
         // EditRequiredPropertyCodePlaceholder
 
         databaseGame.ApplicationUserUpdatedBy = user;
