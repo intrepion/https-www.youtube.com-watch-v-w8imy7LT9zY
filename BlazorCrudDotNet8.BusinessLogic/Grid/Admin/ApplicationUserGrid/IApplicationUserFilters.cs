5d0f4deb126b50a6a0f6650ce53b64dc90060c08
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-10 19:05:04 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorCrudDotNet8\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Grid/Admin/ApplicationUserGrid/*

--- BlazorCrudDotNet8.BusinessLogic/Grid/Admin/ApplicationUserGrid/IApplicationUserFilters.cs
+++ BlazorCrudDotNet8.BusinessLogic/Grid/Admin/ApplicationUserGrid/IApplicationUserFilters.cs
@@ -1,4 +1,4 @@
-﻿namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+﻿namespace BlazorCrudDotNet8.BusinessLogic.Grid.Admin.ApplicationUserGrid;
 
 // Interface for filtering.
 public interface IApplicationUserFilters
