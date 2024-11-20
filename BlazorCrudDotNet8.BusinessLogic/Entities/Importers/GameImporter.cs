a1f2bff8c14ccd9c48871e2b3d398d09daf5d843
Oliver Forral <intrepion@gmail.com>
2024-11-20 15:40:55 -0700

    gawk -i inplace "{ gsub(/\/\/ NewEntityCodePlaceholder/, \"Name = record.Name,\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // NewEntityCodePlaceholder\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Entities/Importers/GameImporter.cs

--- BlazorCrudDotNet8.BusinessLogic/Entities/Importers/GameImporter.cs
+++ BlazorCrudDotNet8.BusinessLogic/Entities/Importers/GameImporter.cs
@@ -58,6 +58,7 @@ public static class GameImporter
                 {
                     ApplicationUserUpdatedBy = applicationUserUpdatedBy,
 
+                    Name = record.Name,
                     // NewEntityCodePlaceholder
                 };
 
