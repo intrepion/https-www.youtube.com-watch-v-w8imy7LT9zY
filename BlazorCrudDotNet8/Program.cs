c01e93981a32403f7d442e5a842aa13fa775eed5
Oliver Forral <intrepion@gmail.com>
2024-11-21 20:25:18 -0700

    gawk -i inplace "{ gsub(/\/\/ RegisterServerServiceCodePlaceholder/, \"builder.Services.AddScoped<IGameFilters, GameGridControls>();\nbuilder.Services.AddScoped<GameGridQueryAdapter>();\n\n// RegisterServerServiceCodePlaceholder\") }; { print }" BlazorCrudDotNet8/Program.cs

--- BlazorCrudDotNet8/Program.cs
+++ BlazorCrudDotNet8/Program.cs
@@ -70,6 +70,9 @@ builder.Services.AddScoped<ApplicationRoleGridQueryAdapter>();
 builder.Services.AddScoped<IApplicationUserFilters, ApplicationUserGridControls>();
 builder.Services.AddScoped<ApplicationUserGridQueryAdapter>();
 
+builder.Services.AddScoped<IGameFilters, GameGridControls>();
+builder.Services.AddScoped<GameGridQueryAdapter>();
+
 // RegisterServerServiceCodePlaceholder
 
 var app = builder.Build();
