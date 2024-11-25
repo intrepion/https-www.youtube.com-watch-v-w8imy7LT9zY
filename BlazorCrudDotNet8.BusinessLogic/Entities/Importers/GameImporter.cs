163d3e63b32c897d46a7b8ba8aa4595bceaa7446
Oliver Forral <intrepion@gmail.com>
2024-11-25 15:36:20 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"game\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Entities/Importers/GameImporter.cs

--- BlazorCrudDotNet8.BusinessLogic/Entities/Importers/GameImporter.cs
+++ BlazorCrudDotNet8.BusinessLogic/Entities/Importers/GameImporter.cs
@@ -54,7 +54,7 @@ public static class GameImporter
                 // NullCheckCodePlaceholder
             )
             {
-                var LowercaseNamePlaceholder = new Game
+                var game = new Game
                 {
                     ApplicationUserUpdatedBy = applicationUserUpdatedBy,
 
@@ -68,7 +68,7 @@ public static class GameImporter
 
                 if (dbGame is null)
                 {
-                    await context.TableNamePlaceholder.AddAsync(LowercaseNamePlaceholder);
+                    await context.TableNamePlaceholder.AddAsync(game);
                 }
                 else
                 {
