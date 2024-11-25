da3376ff5ec76c3351e2c628a5067403caf8dd5a
Oliver Forral <intrepion@gmail.com>
2024-11-25 15:36:56 -0700

    rm -rf BlazorCrudDotNet8/Data

--- BlazorCrudDotNet8/Data/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace BlazorCrudDotNet8.Data;
-
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser
-{
-}
-
