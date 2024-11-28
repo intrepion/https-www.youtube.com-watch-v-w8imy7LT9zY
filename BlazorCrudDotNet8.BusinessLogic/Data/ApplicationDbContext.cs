70d17df4b497289324ce7c97b36e9a9731a55cc8
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:53:38 -0700

    gawk -i inplace "{ gsub(/\/\/ DbSetCodePlaceholder/, \"public DbSet<Game>? Games { get; set; }\n\ \ \ \ // DbSetCodePlaceholder\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Data/ApplicationDbContext.cs

--- BlazorCrudDotNet8.BusinessLogic/Data/ApplicationDbContext.cs
+++ BlazorCrudDotNet8.BusinessLogic/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace ApplicationNamePlaceholder.BusinessLogic.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid, ApplicationUserClaim, ApplicationUserRole, ApplicationUserLogin, ApplicationRoleClaim, ApplicationUserToken>(options)
 {
+    public DbSet<Game>? Games { get; set; }
     // DbSetCodePlaceholder
 
     protected override void OnModelCreating(ModelBuilder builder)
