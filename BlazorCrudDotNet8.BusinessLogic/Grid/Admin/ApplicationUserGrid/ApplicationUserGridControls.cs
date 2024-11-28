425ff034c0fa2a304983e5a7c857d2cf889850ba
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:55:01 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorCrudDotNet8\") }; { print }" BlazorCrudDotNet8.BusinessLogic/Grid/Admin/ApplicationUserGrid/*

--- BlazorCrudDotNet8.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridControls.cs
+++ BlazorCrudDotNet8.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridControls.cs
@@ -1,4 +1,4 @@
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+namespace BlazorCrudDotNet8.BusinessLogic.Grid.Admin.ApplicationUserGrid;
 
 public class ApplicationUserGridControls(IPageHelper pageHelper) : IApplicationUserFilters
 {
--- BlazorCrudDotNet8.BusinessLogic/Grid/Admin/ApplicationUserGrid/IApplicationUserFilters.cs
+++ BlazorCrudDotNet8.BusinessLogic/Grid/Admin/ApplicationUserGrid/IApplicationUserFilters.cs
@@ -1,4 +1,4 @@
-﻿namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+﻿namespace BlazorCrudDotNet8.BusinessLogic.Grid.Admin.ApplicationUserGrid;
 
 public interface IApplicationUserFilters
 {
