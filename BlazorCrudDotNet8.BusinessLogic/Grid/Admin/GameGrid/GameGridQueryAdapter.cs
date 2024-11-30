f5189ec61e88bd1622534802d91bcef4c97494a3
Oliver Forral <intrepion@gmail.com>
2024-11-30 16:42:57 -0700

    dotnet ef --project BlazorCrudDotNet8.BusinessLogic --startup-project BlazorCrudDotNet8 migrations add Initial

---
+++ BlazorCrudDotNet8.BusinessLogic/Grid/Admin/GameGrid/GameGridQueryAdapter.cs
@@ -0,0 +1,75 @@
+using System.Diagnostics;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using BlazorCrudDotNet8.BusinessLogic.Entities;
+
+namespace BlazorCrudDotNet8.BusinessLogic.Grid.Admin.GameGrid;
+
+public class GameGridQueryAdapter
+{
+    private readonly IGameFilters controls;
+
+    private readonly Dictionary<GameFilterColumns, Expression<Func<Game, string>>> expressions =
+        new()
+        {
+            { GameFilterColumns.Id, x => !x.Id.Equals(Guid.Empty) ? x.Id.ToString() : string.Empty },
+
+            { GameFilterColumns.Name, x => x != null && x.Name != null ? x.Name : string.Empty },
+            // SortExpressionCodePlaceholder
+        };
+
+    private readonly Dictionary<GameFilterColumns, Func<IQueryable<Game>, IQueryable<Game>>> filterQueries = [];
+
+    public GameGridQueryAdapter(IGameFilters controls)
+    {
+        this.controls = controls;
+
+        filterQueries =
+            new()
+            {
+                { GameFilterColumns.Id, x => x.Where(y => y != null && !y.Id.Equals(Guid.Empty) && this.controls.FilterText != null && y.Id.ToString().Contains(this.controls.FilterText) ) },
+
+                // QueryExpressionCodePlaceholder
+            };
+    }
+
+    public async Task<ICollection<Game>> FetchAsync(IQueryable<Game> query)
+    {
+        query = FilterAndQuery(query);
+        await CountAsync(query);
+        var collection = await FetchPageQuery(query).ToListAsync();
+        controls.PageHelper.PageItems = collection.Count;
+        return collection;
+    }
+
+    public async Task CountAsync(IQueryable<Game> query) =>
+        controls.PageHelper.TotalItemCount = await query.CountAsync();
+
+    public IQueryable<Game> FetchPageQuery(IQueryable<Game> query) =>
+        query
+            .Skip(controls.PageHelper.Skip)
+            .Take(controls.PageHelper.PageSize)
+            .AsNoTracking();
+
+    private IQueryable<Game> FilterAndQuery(IQueryable<Game> root)
+    {
+        var sb = new System.Text.StringBuilder();
+
+        if (!string.IsNullOrWhiteSpace(controls.FilterText))
+        {
+            var filter = filterQueries[controls.FilterColumn];
+            sb.Append($"Filter: '{controls.FilterColumn}' ");
+            root = filter(root);
+        }
+
+        var expression = expressions[controls.SortColumn];
+        sb.Append($"Sort: '{controls.SortColumn}' ");
+
+        var sortDir = controls.SortAscending ? "ASC" : "DESC";
+        sb.Append(sortDir);
+
+        Debug.WriteLine(sb.ToString());
+
+        return controls.SortAscending ? root.OrderBy(expression) : root.OrderByDescending(expression);
+    }
+}
---
+++ BlazorCrudDotNet8.BusinessLogic/Grid/Admin/GameGrid/IGameFilters.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorCrudDotNet8.BusinessLogic.Grid.Admin.GameGrid;
+
+public interface IGameFilters
+{
+    GameFilterColumns FilterColumn { get; set; }
+
+    bool Loading { get; set; }
+
+    string? FilterText { get; set; }
+
+    IPageHelper PageHelper { get; set; }
+
+    bool SortAscending { get; set; }
+
+    GameFilterColumns SortColumn { get; set; }
+}
