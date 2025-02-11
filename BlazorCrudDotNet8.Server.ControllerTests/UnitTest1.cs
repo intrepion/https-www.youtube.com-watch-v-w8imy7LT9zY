2dc32d6c19120a78e785c7f8ecba5da80292e6e1
Oliver Forral <intrepion@gmail.com>
2025-02-11 01:43:57 -0700

    dotnet format

---
+++ BlazorCrudDotNet8.Server.ControllerTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorCrudDotNet8.Server.ControllerTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
