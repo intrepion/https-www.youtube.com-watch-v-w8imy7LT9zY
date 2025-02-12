80eefceaff143d81dc49f1ca163e6d3bed7dc3bd
Oliver Forral <intrepion@gmail.com>
2025-02-12 12:33:41 -0700

    dotnet ef --project BlazorCrudDotNet8.BusinessLogic --startup-project BlazorCrudDotNet8 migrations add Initial

---
+++ BlazorCrudDotNet8.AcceptanceTests/Admin/GameAdminPages.cs
@@ -0,0 +1,63 @@
+﻿using Bogus;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace BlazorCrudDotNet8.AcceptanceTests.Admin;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public partial class GameAdminPages : PageTest
+{
+    [Test]
+    public async Task MainNavigation()
+    {
+        var faker = new Faker();
+        var aRandomString = faker.Random.String2(10);
+        var someRandomString = faker.Random.String2(10);
+        await Page.GetByTestId("gameNavLink").ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Game Home");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Game Add");
+
+        await Page.GetByLabel("Name:", new() { Exact = true }).FillAsync("aName" + aRandomString);
+        // CreatePropertyCodePlaceholder
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Game View");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Edit" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Game Edit");
+
+        await Page.GetByLabel("Name:", new() { Exact = true }).FillAsync("someName" + someRandomString);
+        // ModifyPropertyCodePlaceholder
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Game View");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Delete" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Confirm" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Back to Grid" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Game Home");
+    }
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        var baseUrl = Environment.GetEnvironmentVariable("BASE_URL");
+        if (string.IsNullOrEmpty(baseUrl))
+        {
+            baseUrl = "http://localhost:5224";
+        }
+        await Page.GotoAsync(baseUrl);
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Expect(Page).ToHaveTitleAsync("Log in");
+        await Page.GetByTestId("loginEmail").FillAsync("Admin1@BlazorCrudDotNet8.com");
+        await Page.GetByTestId("loginPassword").FillAsync("Admin1@BlazorCrudDotNet8.com");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Logout" }).ClickAsync();
+    }
+}
---
+++ BlazorCrudDotNet8.BusinessLogic/Entities/ApplicationUserToken.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCrudDotNet8.BusinessLogic.Entities;
+
+public class ApplicationUserToken : IdentityUserToken<Guid>
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public DateTime UpdateDateTime { get; set; }
+
+    // ActualPropertyPlaceholder
+}
