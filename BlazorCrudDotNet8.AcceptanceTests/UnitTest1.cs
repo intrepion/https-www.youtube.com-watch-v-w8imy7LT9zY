764e78f003e24ddddfbc63e62c5fc263d4dbacf2
Oliver Forral <intrepion@gmail.com>
2025-03-06 19:40:34 -0700

    dotnet format

---
+++ BlazorCrudDotNet8.AcceptanceTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorCrudDotNet8.AcceptanceTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
