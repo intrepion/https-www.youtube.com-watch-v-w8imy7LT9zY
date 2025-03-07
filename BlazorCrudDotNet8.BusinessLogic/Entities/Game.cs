755002c48658498c0fab27d7c7d22464b5ea1a7b
Oliver Forral <intrepion@gmail.com>
2025-03-06 19:42:37 -0700

    dotnet ef --project BlazorCrudDotNet8.BusinessLogic --startup-project BlazorCrudDotNet8 migrations add Initial

---
+++ BlazorCrudDotNet8.BusinessLogic/Entities/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCrudDotNet8.BusinessLogic.Entities;
+
+public class Game
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public Guid Id { get; set; }
+    public DateTime UpdateDateTime { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string NormalizedName { get; set; } = string.Empty;
+    // ActualPropertyPlaceholder
+}
---
+++ BlazorCrudDotNet8.BusinessLogic/Entities/Importers/GameImporter.cs
@@ -0,0 +1,90 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using BlazorCrudDotNet8.BusinessLogic.Data;
+using BlazorCrudDotNet8.BusinessLogic.Entities.Records;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrudDotNet8.BusinessLogic.Entities.Importers;
+
+public static class GameImporter
+{
+    public static async Task ImportAsync(
+       ApplicationDbContext context,
+       string userName, string csvPath
+    )
+    {
+        if (!File.Exists(csvPath))
+        {
+            Console.WriteLine("File not found: " + csvPath);
+            return;
+        }
+
+        if (context.Games is null)
+        {
+            Console.WriteLine("Database table not found: context.Games");
+            return;
+        }
+
+        var normalizedUserName = userName.ToUpperInvariant();
+        var applicationUserUpdatedBy = await context.Users.SingleOrDefaultAsync(x => x.NormalizedUserName != null && x.NormalizedUserName.Equals(normalizedUserName));
+
+        if (applicationUserUpdatedBy is null)
+        {
+            Console.WriteLine("UserName not found: " + userName);
+            return;
+        }
+
+        using var reader = new StreamReader(csvPath);
+        using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            PrepareHeaderForMatch = x => x.Header.ToUpper(CultureInfo.InvariantCulture),
+            Delimiter = "|",
+        });
+
+        var records = csv.GetRecords<GameRecord>();
+
+        // EntityListCodePlaceholder
+
+        foreach (var record in records)
+        {
+            // ManyToOneCodePlaceholder
+
+            if (true
+                // NullCheckCodePlaceholder
+            )
+            {
+                var game = new Game
+                {
+                    ApplicationUserUpdatedBy = applicationUserUpdatedBy,
+                    UpdateDateTime = DateTime.UtcNow,
+
+                    Name = record.Name,
+                    NormalizedName = record.Name.ToUpperInvariant(),
+                    // NewEntityCodePlaceholder
+                };
+
+                var dbGame = await context.Games.SingleOrDefaultAsync(
+                    x => true
+                    && x.NormalizedName.Equals(game.NormalizedName)
+                    // CompositeKeyCodePlaceholder
+                );
+
+                if (dbGame is null)
+                {
+                    await context.Games.AddAsync(game);
+                }
+                else
+                {
+                    dbGame.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
+                    dbGame.UpdateDateTime = DateTime.UtcNow;
+
+                    dbGame.Name = record.Name;
+                    // ExistingEntityCodePlaceholder
+                }
+            }
+        }
+
+        await context.SaveChangesAsync();
+    }
+}
